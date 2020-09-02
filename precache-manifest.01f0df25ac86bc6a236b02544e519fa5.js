2cb712d25306c20ce4ddda4fd05158b589463c98
patnikorn tai-ngoen <patnikorn.ta.61@ubu.ac.th>
2020-09-02 12:29:05 +0700

    Updates

---
+++ precache-manifest.01f0df25ac86bc6a236b02544e519fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426a4ddfe2b5111a48d0c09c66c1db14",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ccd3536e22856c427f63",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "42791388e1b87353b0b0",
+    "url": "/tic-tac-toe/static/js/2.76e9d1fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.76e9d1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd3536e22856c427f63",
+    "url": "/tic-tac-toe/static/js/main.ec22e34b.chunk.js"
+  },
+  {
+    "revision": "85e21c647aef003b71a2",
+    "url": "/tic-tac-toe/static/js/runtime-main.fb7a4dc5.js"
+  }
+]);
